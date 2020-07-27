ffd3225a1f3102659336e7c10589681b2e602bd7
YusAm11 <yusuf_a20@yahoo.com>
2020-07-26 23:22:15 -0700

    Update DIIWallpaper.java

--- DIIWallpaper.java
+++ DIIWallpaper.java
@@ -5,54 +5,72 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 import javax.script.ScriptEngineFactory;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.win32.W32APIOptions;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+ 
+import javax.imageio.ImageIO;
 
 public class DIIWallpaper {
 
-  //The OS of the computer we're using
-  private static String OS = System.getProperty("os.name").toLowerCase();
+  private static String OS = System.getProperty("os.name").toLowerCase(); //The OS of the computer we're using
 
   public static void main(String[] args) {
 
+    BufferedImage image = ImageIO.read(new File("C:\\Users\\yusufpc\\Pictures\\Hadith-Intentions.png"));
+
+    Graphics g = image.getGraphics();
+    g.setFont(g.getFont().deriveFont(30f));
+    g.drawString("Hello World!", 100, 100);
+    g.dispose();
+      
     //Changes wallpaper to the Ayah talking about the Mercy to the Worlds (SAWS)
-    changeWallpaper("/Users/shaheer/Documents/GitHub/Daily-Islamic-Inspiration-Wallpaper/Wallpapers/Ayah-Mercy-To-Worlds.png");
+    changeWallpaper("C:\\Users\\yusufpc\\Pictures\\Hadith-Intentions.png");
 
   }
-
+  public static interface User32 extends Library {
+        User32 INSTANCE = (User32) Native.loadLibrary("user32",User32.class,W32APIOptions.DEFAULT_OPTIONS);
+        boolean SystemParametersInfo (int one, int two, String s ,int three);
+  }
   //Change wallpaper to file specified by path
   public static void changeWallpaper(String path) {
+
+    //For Mac
     if(isMac()){
+
       try {
+
           ProcessBuilder pb = new ProcessBuilder("osascript", "-e",
               "tell application \"Finder\" to set desktop picture to POSIX file \"" + path + "\"");
           Process p = pb.start();
+
       } catch (Exception ex) {
+
           ex.printStackTrace();
+
       }
-    }
-    else if(isWindows()){
-      try{
-        //ADD CODE FOR CHANGING WALLPAPER ON WINDOWS THROUGHT COMMAND PROMPT
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    }
-    else{
+
+    } else if(isWindows()){ //For Windows
+
+        try{        
+          User32.INSTANCE.SystemParametersInfo(0x0014, 0, path , 1);          
+        } catch (Exception ex) {
+
+          ex.printStackTrace();
+
+        }
+
+    } else{ //Error message if any other operating system
+
       System.out.println("Daily Islamic Inspiration Wallpaper currently does not support your OS");
+
     }
 
-  }
-  public static void changeWindowsWallpaper(String path) {
-	String command1 = "reg add \"HKEY_CURRENT_USER\\Control Panel\\Desktop\" /v Wallpaper /t REG_SZ /d  C:\\Users\\yusufpc\\Downloads\testbackground1.bmp /f";
-	String command2 = "RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters";
- 
-	try {
-		Process process1 = Runtime.getRuntime().exec(command1);
-		Process process2 = Runtime.getRuntime().exec(command2);
-	 
-	 
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
   }
 
   //Checks if computer uses Windows
@@ -69,5 +87,5 @@ public class DIIWallpaper {
 
 	}
 
-}
 
+}

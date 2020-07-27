c898077fef0843aee0b989cbadb6d7fd9ecb5c2a
Shaheer <shahimr1234@gmail.com>
2020-07-26 18:29:06 -0700

    Groundwork for Mac/Windows compatibility
    -Added isWindows()
    -Added isMac()
    -Added if/else if statements in changeWallpaper() to check for OS version

--- DIIWallpaper.java
+++ DIIWallpaper.java
@@ -8,6 +8,9 @@ import javax.script.ScriptEngineFactory;
 
 public class DIIWallpaper {
 
+  //The OS of the computer we're using
+  private static String OS = System.getProperty("os.name").toLowerCase();
+
   public static void main(String[] args) {
 
     //Changes wallpaper to the Ayah talking about the Mercy to the Worlds (SAWS)
@@ -17,14 +20,40 @@ public class DIIWallpaper {
 
   //Change wallpaper to file specified by path
   public static void changeWallpaper(String path) {
-
-    try {
-        ProcessBuilder pb = new ProcessBuilder("osascript", "-e",
-            "tell application \"Finder\" to set desktop picture to POSIX file \"" + path + "\"");
-        Process p = pb.start();
-    } catch (Exception ex) {
-        ex.printStackTrace();
+    if(isMac()){
+      try {
+          ProcessBuilder pb = new ProcessBuilder("osascript", "-e",
+              "tell application \"Finder\" to set desktop picture to POSIX file \"" + path + "\"");
+          Process p = pb.start();
+      } catch (Exception ex) {
+          ex.printStackTrace();
+      }
     }
+    else if(isWindows()){
+      try{
+        //ADD CODE FOR CHANGING WALLPAPER ON WINDOWS THROUGHT COMMAND PROMPT
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+    }
+    else{
+      System.out.println("Daily Islamic Inspiration Wallpaper currently does not support your OS");
+    }
+
   }
+
+  //Checks if computer uses Windows
+  public static boolean isWindows() {
+
+		return (OS.indexOf("win") >= 0);
+
+	}
+
+  //Checks if computer uses Mac
+	public static boolean isMac() {
+
+		return (OS.indexOf("mac") >= 0);
+
+	}
 
 }
